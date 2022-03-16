c9d5d9964c1f34b33c640f904dbbbb72c443c4c8
achilleswargod <65618715+achilleswargod@users.noreply.github.com>
2022-03-16 13:50:01 -0500

    Updated models.py and serializers.py to Include an image_link for Bonus Challenge 1 + Reformated Project Using Class Based Views

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.products_list),
-    path('<int:pk>/', views.product_detail),
+    path('', views.ProductList.as_view()),
+    path('<int:pk>/', views.ProductDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+#OLD CODE
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.products_list),
+#     path('<int:pk>/', views.product_detail),
+# ]
